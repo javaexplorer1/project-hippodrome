a3baf41c51ac6eb20f7dc0bb80361a82f75c26f0
Dmitriy <dmed832@yandex.ru>
2024-01-06 23:56:38 +0300

    add test classes

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    @Test
+    void getHorses() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorseTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getRandomDouble() {
+    }
+}

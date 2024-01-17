d6fe4a1f7adaa6d88ce5c2bae4657b4f5feef286
Dmitriy <dmed832@yandex.ru>
2024-01-17 23:42:42 +0300

    add test methods

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,8 +1,35 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
+
+
+    @Test
+    void testHorseConstructorException() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    void testHorseConstructorExceptionMessage() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+        assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+
+    @Test
+    void testHorseConstructorExceptionEmptyList() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+    }
+
+
+    @Test
+    void testHorseConstructorExceptionEmptyListMessage() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+        assertEquals("Horses cannot be empty.", exception.getMessage());
+    }
+
 
     @Test
     void getHorses() {

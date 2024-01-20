96d6f1d64119efa9843fb4adf4434de2d8d6aa2b
Dmitriy <dmed832@yandex.ru>
2024-01-20 03:25:55 +0300

    add test methods

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,18 +39,30 @@ class HippodromeTest {
     @Test
     void getHorses() {
 
-        List<Horse> expectedList = new LinkedList<>();
-        for (int i = 0; i < 30; i++) {
-            expectedList.add(new Horse("Horse" + i, 1.0));
-        }
-        assertEquals(expectedList, (new Hippodrome(expectedList)).getHorses());
+//        List<Horse> expectedList = new LinkedList<>();
+//        for (int i = 0; i < 30; i++) {
+//            expectedList.add(new Horse("Horse" + i, 1.0));
+//        }
+        List<Horse> horses = IntStream.range(0, 30).mapToObj(i -> new Horse("Horse" + i, i)).toList();
+        Hippodrome hippodrome = new Hippodrome(horses);
+        assertEquals(horses, hippodrome.getHorses());
+//        assertSame(horses, hippodrome.getHorses());
     }
 
     @Test
     void move() {
+        List<Horse> horses = IntStream.range(0, 50).mapToObj(i -> Mockito.mock(Horse.class)).toList();
+        new Hippodrome(horses).move();
+        horses.forEach(horse -> Mockito.verify(horse, Mockito.times(1)).move());
     }
 
     @Test
     void getWinner() {
+        Horse horsesOne = new Horse("Horse1", 1, 10);
+        Horse horsesTwo = new Horse("Horse2", 1, 20);
+        Horse horsesThree = new Horse("Horse3", 1, 30);
+
+        Hippodrome hippodrome = new Hippodrome(List.of(horsesOne, horsesTwo, horsesThree));
+        assertSame(horsesThree, hippodrome.getWinner());
     }
 }

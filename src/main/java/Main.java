1725e9fbe05fded100e22228850ecb3153b3a9e8
Dmitriy <dmed832@yandex.ru>
2024-01-27 23:45:20 +0300

    add logging + configuration file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Main {
+
     @SneakyThrows
     public static void main(String[] args) {
+
         List<Horse> horses = List.of(
                 new Horse("Bucephalus", 2.4),
                 new Horse("Ace of Spades", 2.5),
@@ -16,6 +20,7 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        log.info("Начало скачек. Количество участников: {}", horses.size());
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();

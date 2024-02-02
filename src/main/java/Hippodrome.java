01922cb6e7fd7cf7b0931b5be4f4af94c797cbde
Dmitriy <dmed832@yandex.ru>
2024-02-02 21:01:42 +0300

    add logs

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,21 +1,27 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import static java.util.Objects.isNull;
 
+@Slf4j
 public class Hippodrome {
 
     private final List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            log.info("ERROR Hippodrome: Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            log.info("ERROR Hippodrome: Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        log.info("DEBUG Hippodrome: Создание Hippodrome, лошадей [{}]", horses.size());
     }
 
     public List<Horse> getHorses() {
--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,5 +1,8 @@
+import lombok.extern.slf4j.Slf4j;
+
 import static java.util.Objects.isNull;
 
+@Slf4j
 public class Horse {
 
     private final String name;
@@ -8,20 +11,25 @@ public class Horse {
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            log.info("ERROR Horse: Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            log.info("ERROR Horse: Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            log.info("ERROR Horse: Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            log.info("ERROR Horse: Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        log.info("DEBUG Horse: Создание Horse, имя [{}}], скорость [{}]", name, speed);
     }
 
     public Horse(String name, double speed) {

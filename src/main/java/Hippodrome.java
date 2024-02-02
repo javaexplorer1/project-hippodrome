112a63a6c8447899c5b5599d281f129317582c3d
Dmitriy <dmed832@yandex.ru>
2024-02-02 23:48:01 +0300

    update Hippodrome

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,21 +1,27 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import static java.util.Objects.isNull;
 
+@Slf4j
 public class Hippodrome {
 
     private final List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            log.error("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            log.error("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        log.debug("Создание Hippodrome, лошадей [{}]", horses.size());
     }
 
     public List<Horse> getHorses() {

daa9b2e04572aa690e3be61b8ea7363b8bd441bd
Dmitriy <dmed832@yandex.ru>
2024-02-02 23:48:38 +0300

    update Horse

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,5 +1,8 @@
+import lombok.extern.slf4j.Slf4j;
+
 import static java.util.Objects.isNull;
 
+@Slf4j
 public class Horse {
 
     private final String name;
@@ -8,20 +11,25 @@ public class Horse {
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            log.error("Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            log.error("Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            log.error("Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            log.error("Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        log.debug("Создание Horse, имя [{}}], скорость [{}]", name, speed);
     }
 
     public Horse(String name, double speed) {
